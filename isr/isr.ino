7c4dad648419c32999337bc62d3b5bead6cfb1d5
Ryley McRae <ryleymcrae@gmail.com>
2023-02-21 11:16:24 -0800

    updated isr example

--- isr/isr.ino
+++ isr/isr.ino
@@ -2,10 +2,12 @@ const int led_pin = 2;
 hw_timer_t *My_timer = NULL;
 
 void IRAM_ATTR onTimer() {
-  Seriprint(millis());
+  long curr = millis();
+  Serial.println(curr);
 }
 
 void setup() {
+  Serial.begin(115200);
   pinMode(led_pin, OUTPUT);
   My_timer = timerBegin(0, 80, true);
   timerAttachInterrupt(My_timer, &onTimer, true);
